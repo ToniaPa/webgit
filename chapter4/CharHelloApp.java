339819902f04b8c71f1efa8268839414827bbf10
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2022-12-13 14:45:35 +0200

    chapter4 java language

---
+++ chapter4/CharHelloApp.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.chapter4;
+
+/**
+ * print Hello using chars
+ */
+public class CharHelloApp {
+
+    public static void main(String[] args) {
+        char h = 'H';
+        char e = 'e';
+        char l = 'l';
+        char o = 'o';
+        char exclamationMark = '!';
+
+        System.out.print(h);
+        System.out.print(e);
+        System.out.print(l);
+        System.out.print(l);
+        System.out.print(o);
+        System.out.print(exclamationMark);
+    }
+}
