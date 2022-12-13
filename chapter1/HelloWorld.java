95f4d52c969f34b9ad5010160bb9c8714bd4ad98
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2022-12-13 14:41:46 +0200

    chapter1 java language

---
+++ chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.chapter1;
+
+/**
+ * Η πρώτη μου class
+ * εκτυπώνει Hello World στο std-output
+ * @aveMoi=Tonia
+ */
+public class HelloWorld{
+    public static void main(String[] args) {
+        //std-output
+        System.out.println("Hello World");
+    }
+}
